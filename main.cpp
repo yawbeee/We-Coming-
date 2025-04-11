1eba7ba65ae576552085eb83b5086f0ea759c16e
Yawbee49 <oforiaduy@gmail.com>
2025-04-11 11:50:45 +0000

    Change

--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
-#include <iostream> //Line 1
-using namespace std; //Line 2
-int main() //Line 3
-{ //Line 4
-    char ch; //Line 5
-    cout << "Line 6: Enter a string: "; //Line 6
-    cin.get(ch); //Line 7
-    cout << endl; //Line 8
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout << "Line 6: Enter a string: ";
+    cin.get(ch);
+    cout << endl;
     cout << "Line 9: After first cin.get(ch); "
-    << "ch = " << ch << endl; //Line 9
-    cin.get(ch); //Line 10
+    << "ch = " << ch << endl;
+    cin.get(ch);
     cout << "Line 11: After second cin.get(ch); "
-    << "ch = " << ch << endl; //Line 11
-    cin.putback(ch); //Line 12
-    cin.get(ch); //Line 13
-    cout << "Line 14: After putback and then "<< "cin.get(ch); ch = " << ch << endl; //Line 14
-    ch = cin.peek(); //Line 15
+    << "ch = " << ch << endl;
+    cin.putback(ch);
+    cin.get(ch);
+    cout << "Line 14: After putback and then "<< "cin.get(ch); ch = " << ch << endl;
+    ch = cin.peek();
     cout << "Line 16: After cin.peek(); ch = "
-    << ch << endl; //Line 16
-    cin.get(ch); //Line 17
+    << ch << endl;
+    cin.get(ch);
     cout << "Line 18: After cin.get(ch); ch = "
-    << ch << endl; //Line 18
-    return 0; //Line 19
+    << ch << endl;
+    return 0; 
 }
 
 
